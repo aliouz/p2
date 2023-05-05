d62a4102ec38374e24cdb191d4ad1dd67d1bbdb0
aliouz <123784505+aliouz@users.noreply.github.com>
2023-05-04 22:19:07 -0700

    Update queue_tester_example.c

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -59,10 +59,39 @@ void test_destroy(void)
 	
 }
 
+
+static void iterator_inc(queue_t q, void *data)
+{
+    int *a = (int*)data;
+
+    if (*a == 42)
+        queue_delete(q, data);
+    else
+        *a += 1;
+}
+
+void test_iterator(void)
+{
+    queue_t q;
+    int data[] = {1, 2, 3, 4, 5, 42, 6, 7, 8, 9};
+    size_t i;
+
+    /* Initialize the queue and enqueue items */
+    q = queue_create();
+    for (i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+        queue_enqueue(q, &data[i]);
+
+    /* Increment every item of the queue, delete item '42' */
+    queue_iterate(q, iterator_inc);
+    assert(data[0] == 2);
+    assert(queue_length(q) == 9);
+}
+
 int main(void)
 {
 	test_create();
 	test_queue_simple();
 	test_destroy();
+	test_iterator();
 	return 0;
 }
